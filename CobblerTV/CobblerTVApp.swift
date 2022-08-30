f002ce6bcc9de533c966e2f4dabf4645082227fe
Simo Virokannas <simo@virokannas.fi>
2022-08-30 17:12:52 -0400

    Apple TV target

---
+++ CobblerTV/CobblerTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobblerTVApp.swift
+//  CobblerTV
+//
+//  Created by Simo Virokannas on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CobblerTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
